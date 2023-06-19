1fc0cd7fd1f47ae7cf643f3f08a04b745da382a7
Suraj Wate <suraj.wate@gmail.com>
2023-06-20 02:53:18 +0530

    404 Templates
    Created 404.html template

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -48,4 +48,5 @@ def monthly_challenge(request, month):
             "month": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        response_data = render_to_string('404.html')
+        return HttpResponseNotFound(response_data)
